133b86578d3f877f354f6c9611a21cff7e405f4d
Syed Maaz Hussaini <skr2570@gmail.com>
2022-09-19 00:19:16 +0500

    update transformation file.

--- V 2.0/transformation.py
+++ V 2.0/transformation.py
@@ -5,75 +5,328 @@ import json
 
 
 def DatabaseConnection():
-    conn = sql.connect(host="SERVER_NAME",
-                            user="admin", password="PASSWORD", database="database")
+    conn = sql.connect(host="rawiikodata.cpi0e90jeqda.us-east-2.rds.amazonaws.com",
+                            user="admin", password="MySql12$34#", database="rawDataTest")
 
     return conn
 
 
 conn = DatabaseConnection()
+cursor = conn.cursor()
 
 
-f = open("config.json")
-configData = json.load(f)
+class TransformationRawData:
 
-iikoStoreMapping = pd.read_excel(
-    configData['SourceFolder2']+"Copy-Transformation_rawHeader.xlsx", sheet_name="iikoStoreMapping")
+    def transformHeader(self):
 
-iikoStoreMapping.rename(
-    columns={
-        'rawHeaderValue': 'store',
-        "transHeaderValuePhysicalStoreNo": "storeNo"
-    }, inplace=True
-)
+        query = """
+            with rawHeaderFetchData as (
+                SELECT
+                    pk_rawHeader as fk_rawHeader, uniqueOrderId, store, conception, DATE_FORMAT(accountingDay,'%d/%m/%Y') as accountingDay,
+                    receiptNo, orderDeleted, delivery, GrossSalesBD, grossSalesAD, discountAmount, surchargeAmount,
+                    (GrossSalesBD - grossSalesAD) as effectiveDiscount, VATByBillAmount as VAT, NetSalesAD as NetSales,
+                    numberOfGuests as NoOfCovers, (GrossSalesBD - VATByBillAmount) as GGGross,
+                    SUBSTRING_INDEX(openingTime, 'T', -1) as OpeningTime, tableNo, shiftNo, deliveryType,
+                    section, externalOrderNo, customerPhoneNumber, customerFullName, customerPriceCategory,
+                    deliveryComment, waiterForTheOrder as orderWaiter, deliveryPrintTime, plannedDeliveryTime,
+                    deliveryDispatchTime, actualDeliveryTime, deliveryClosingTime, guestBillTime, deliveryInvoicePrintTime,
+                    closingTime, numberOfIitems, numberOfDiscountedItems, numberOfComboDiscounts, averageRevenuePerGuest,
+                    lastServPrintTime,
+                    CASE
+                        WHEN (deliverySource != '') and (
+                            deliverySource != 'CloudCallCenter')
+                            Then deliverySource
+                        else
+                            orderType
+                    END AS RefinedOrderSource,
+                    CASE
+                        WHEN deliverySource = 'CloudCallCenter'
+                            THEN 'CC'
+                        ELSE
+                            'NO'
+                    END AS OriginatedCCYN
+                FROM `rawDataTest`.`rawHeader` WHERE orderDeleted = 'NOT_DELETED' and store != 'Test Lab'
+            )
 
-iikoBrandMapping = pd.read_excel(
-    configData['SourceFolder2']+"Copy-Transformation_rawHeader.xlsx", sheet_name="iikoBrandMapping")
+            SELECT 
+                rawHeaderFetchData.fk_rawHeader, rawHeaderFetchData.uniqueOrderId, rawHeaderFetchData.store, 
+                rawHeaderFetchData.conception, rawHeaderFetchData.accountingDay, rawHeaderFetchData.receiptNo, 
+                rawHeaderFetchData.orderDeleted, rawHeaderFetchData.delivery, rawHeaderFetchData.GrossSalesBD, 
+                rawHeaderFetchData.grossSalesAD, rawHeaderFetchData.discountAmount, rawHeaderFetchData.surchargeAmount,
+                rawHeaderFetchData.effectiveDiscount, rawHeaderFetchData.VAT, rawHeaderFetchData.NetSales, 
+                rawHeaderFetchData.NoOfCovers, rawHeaderFetchData.GGGross, rawHeaderFetchData.OpeningTime, rawHeaderFetchData.tableNo, 
+                rawHeaderFetchData.shiftNo, rawHeaderFetchData.deliveryType, rawHeaderFetchData.section, 
+                rawHeaderFetchData.externalOrderNo, rawHeaderFetchData.customerPhoneNumber, 
+                rawHeaderFetchData.customerFullName, rawHeaderFetchData.customerPriceCategory, rawHeaderFetchData.deliveryComment, 
+                rawHeaderFetchData.orderWaiter, rawHeaderFetchData.deliveryPrintTime, rawHeaderFetchData.plannedDeliveryTime,
+                rawHeaderFetchData.deliveryDispatchTime, rawHeaderFetchData.actualDeliveryTime, 
+                rawHeaderFetchData.deliveryClosingTime, rawHeaderFetchData.guestBillTime, rawHeaderFetchData.deliveryInvoicePrintTime,
+                rawHeaderFetchData.closingTime, rawHeaderFetchData.numberOfIitems, 
+                rawHeaderFetchData.numberOfDiscountedItems, rawHeaderFetchData.numberOfComboDiscounts, 
+                rawHeaderFetchData.averageRevenuePerGuest, rawHeaderFetchData.lastServPrintTime, rawHeaderFetchData.OriginatedCCYN,
+                CASE
+                    WHEN rawHeaderFetchData.RefinedOrderSource = 'CALL CENTER'
+                        THEN 'CALL'
+                    ELSE
+                        rawHeaderFetchData.RefinedOrderSource
+                END as RefinedOrderSource,
 
-iikoBrandMapping.rename(
-    columns={
-        'rawHeaderValue': 'conception'
-    }, inplace=True
-)
+                iikoStoreMapping.storeNo, iikoBrandMapping.conceptionNo,
+                iikoVirtualStoreMapping.storeNo as virtualStoreNo, iikoVirtualStoreMapping.virtualStoreName,
+                iikoVirtualStoreMapping.financeStoreNo, iikoIsDeliveryMapping.deliveryNo, iikoStoreMapping.regionCode
 
-iikoVirtualStoreMapping = pd.read_excel(
-    configData['SourceFolder2']+"Copy-Transformation_rawHeader.xlsx", sheet_name="iikoVirtualStoreMapping")
+            from rawHeaderFetchData
+                inner join `rawDataTest`.`iikoStoreMapping` as iikoStoreMapping
+                    on rawHeaderFetchData.store = iikoStoreMapping.storeName
+                inner join `rawDataTest`.`iikoBrandMapping` as iikoBrandMapping
+                    on rawHeaderFetchData.conception = iikoBrandMapping.conceptionName
+                inner join `rawDataTest`.`iikoVirtualStoreMapping` as iikoVirtualStoreMapping
+                    on (
+                            rawHeaderFetchData.store = iikoVirtualStoreMapping.storeName
+                                and
+                            rawHeaderFetchData.conception = iikoVirtualStoreMapping.conceptionName
+                        )
+                inner join `rawDataTest`.`iikoIsDeliveryMapping` as iikoIsDeliveryMapping
+                    on rawHeaderFetchData.delivery = iikoIsDeliveryMapping.deliveryName
+            ;
+        """
 
-iikoVirtualStoreMapping.rename(
-    columns={
-        'rawHeaderValueDepartment': 'store',
-        'rawHeaderValueConception': 'conception'
-    }, inplace=True
-)
+        rawHeaderTransformData = pd.read_sql(query, conn)
+        rawHeaderTransformData['salesTransformed'] = False
+        rawHeaderTransformData['paymentTransformed'] = False
 
-rawHeaderFetchData = pd.read_sql(
-    "SELECT pk_rawHeader, store, conception, DATE_FORMAT(accountingDay,'%d/%m/%Y') as accountingDay FROM `rawDataTest`.`rawHeader` WHERE orderDeleted = 'NOT_DELETED' and store != 'Test Lab'", conn)
+        rawHeaderTransformData = rawHeaderTransformData[[
+            'fk_rawHeader', 'uniqueOrderId', 'store', 'conception', 'virtualStoreNo', 'virtualStoreName',
+            'financeStoreNo', 'accountingDay', 'receiptNo', 'deliveryNo', 'RefinedOrderSource', 'OriginatedCCYN',
+            'GrossSalesBD', 'discountAmount', 'surchargeAmount', 'grossSalesAD', 'effectiveDiscount', 'VAT',
+            'NetSales', 'NoOfCovers', 'GGGross', 'regionCode', 'OpeningTime', 'tableNo', 'shiftNo', 'deliveryType',
+            'section', 'externalOrderNo', 'customerPhoneNumber', 'customerFullName', 'deliveryComment', 'orderWaiter',
+            'deliveryPrintTime', 'plannedDeliveryTime', 'deliveryDispatchTime', 'actualDeliveryTime',
+            'deliveryClosingTime', 'guestBillTime', 'deliveryInvoicePrintTime', 'closingTime', 'customerPriceCategory',
+            'numberOfIitems', 'numberOfDiscountedItems', 'numberOfComboDiscounts', 'averageRevenuePerGuest',
+            'lastServPrintTime', 'salesTransformed', 'paymentTransformed'
+        ]]
 
-print(rawHeaderFetchData.shape)
+        print(rawHeaderTransformData.shape)
 
-iikoStoreTransformData = pd.merge(
-    rawHeaderFetchData, iikoStoreMapping.store, how='inner')
+        rawHeaderTransformData_toList = list(tuple(row)
+                                             for row in rawHeaderTransformData.values)
 
-# print(iikoStoreMapping.loc[iikoStoreMapping.store.isin(
-#     list(iikoStoreTransformData.store))])
+        insertQuery = """ INSERT INTO `transformedData`.`transformHeader`
+                (
+                    `fk_rawHeader`,
+                    `uniqueOrderId`,
+                    `store`,
+                    `conception`,
+                    `virtualStoreNo`,
+                    `virtualStoreName`,
+                    `financeStoreNo`,
+                    `accountingDay`,
+                    `receiptNo`,
+                    `deliveryNo`,
+                    `RefinedOrderSource`,
+                    `OriginatedCCYN`,
+                    `GrossSalesBD`,
+                    `discountAmount`,
+                    `surchargeAmount`,
+                    `grossSalesAD`,
+                    `effectiveDiscount`,
+                    `VAT`,
+                    `NetSales`,
+                    `NoOfCovers`,
+                    `GGGross`,
+                    `regionCode`,
+                    `OpeningTime`,
+                    `tableNo`,
+                    `shiftNo`,
+                    `deliveryType`,
+                    `section`,
+                    `externalOrderNo`,
+                    `customerPhoneNumber`,
+                    `customerFullName`,
+                    `deliveryComment`,
+                    `orderWaiter`,
+                    `deliveryPrintTime`,
+                    `plannedDeliveryTime`,
+                    `deliveryDispatchTime`,
+                    `actualDeliveryTime`,
+                    `deliveryClosingTime`,
+                    `guestBillTime`,
+                    `deliveryInvoicePrintTime`,
+                    `closingTime`,
+                    `customerPriceCategory`,
+                    `numberOfIitems`,
+                    `numberOfDiscountedItems`,
+                    `numberOfComboDiscounts`,
+                    `averageRevenuePerGuest`,
+                    `lastServPrintTime`,
+                    `salesTransformed`,
+                    `paymentTransformed`
+                )
+                VALUES
+                (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.executemany(insertQuery, rawHeaderTransformData_toList)
+        conn.commit()
+        print("INSERTED INTO transform Header")
 
-iikoStoreTransformData['PhysicalStoreNo'] = ''
+    def transformPayment(self):
+        query = """
+            with rawHeaderFetchData as (
+                SELECT uniqueOrderId FROM `transformedData`.`transformHeader` WHERE paymentTransformed = 0
+            )
 
-for ind1, val1 in iikoStoreMapping.iterrows():
+            SELECT 
+                rawPayment.uniqueOrderId as uniqueOrderId, rawPayment.paymentType as paymentType, DATE_FORMAT(rawPayment.accountingDay,'%d/%m/%Y') as accountingDay,
+                rawPayment.paymentTypeSplit as paymentTypeSplit, rawPayment.paymentCardNo as paymentCardNo, 
+                rawPayment.paymentCurrency as paymentCurrency, rawPayment.nonCashPaymentType as nonCashPaymentType, 
+                rawPayment.creditedTo as creditedTo, rawPayment.paymentExchangeRateAED as exchangeRateAED,
+                rawPayment.authorisedBy as authorisedBy, rawPayment.authorisedByID as authorisedByID, 
+                rawPayment.cashier as cashier, rawPayment.cashierID as cashierID, rawPayment.grossSalesAD as grossSalesAD, 
+                rawPayment.VATByBillAmount as VAT, rawPayment.netSalesAD as netSales, rawPayment.numberOfVouchers as numberOfVouchers
+            FROM `rawDataTest`.`rawPayment` as rawPayment
+            INNER JOIN rawHeaderFetchData
+            on rawHeaderFetchData.uniqueOrderId = rawPayment.uniqueOrderId;
 
-    print(val1['store'])
-    break
-    # for ind2, val2 in iikoStoreTransformData.iterrows():
+        """
+
+        transformPaymentFetchData = pd.read_sql(query, conn)
+
+        transformPaymentFetchData_toList = list(tuple(row)
+                                                for row in transformPaymentFetchData.values)
+
+        insert_Query = """
+            INSERT INTO `transformedData`.`transformPayment`
+                (`uniqueOrderId`,
+                `paymentType`,
+                `accountingDay`,
+                `paymentTypeSplit`,
+                `paymentCardNo`,
+                `paymentCurrency`,
+                `nonCashPaymentType`,
+                `creditedTo`,
+                `exchangeRateAED`,
+                `authorisedBy`,
+                `authorisedByID`,
+                `cashier`,
+                `cashierID`,
+                `grossSalesAD`,
+                `VAT`,
+                `netSales`,
+                `numberOfVouchers`)
+                VALUES
+                (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+        cursor.executemany(insert_Query, transformPaymentFetchData_toList)
+        conn.commit()
+        print("INSERTED INTO transform Payment")
+
+        updateQuery = f"""
+            UPDATE `transformedData`.`transformHeader` SET paymentTransformed = True
+            WHERE uniqueOrderId IN {tuple(transformPaymentFetchData.uniqueOrderId)};
+        """
+        cursor.execute(updateQuery)
+        conn.commit()
+
+        # temp = pd.read_sql(updateQuery, conn)
+        # print(temp)
+
+        print("Update INTO transform Header's paymentTransformed")
+
+    def transformSales(self):
+        query = """
+            with rawSalesFetchData as (
+                SELECT 
+                    uniqueOrderId, store, concept, DATE_FORMAT(accountingDay,'%d/%m/%Y') as accountingDay, receiptNo,
+                    itemCode, itemId, item as itemName, itemFullName, itemAccountingCategory, itemCategory, itemGroup,
+                    courseNo, itemWaiter, itemServicePrinting, itemComment, soldWithItem, discountType, surchargeType,
+                    cookingFinishedAt, numberOfItems, averagePriceBeforeDiscount, averagePrice, grossSalesBD, 
+                    discountAmount, surchargeAmountAED, grossSalesAD, (grossSalesBD - grossSalesAD) as effectiveDiscount,
+                    VATByBillAmount as VAT, netSalesAD as netSales, cookingTimeAverage, cookingTimeAverage1, cookingTimeAverage2,
+                    cookingTimeAverage3, cookingTimeAverage4
+                FROM `rawDataTest`.`rawSales`
+                WHERE rawSales.itemDeleted = 'NOT_DELETED'
+            )
+
+            SELECT rawSales.*, iikoStoreMapping.storeNo, iikoBrandMapping.conceptionNo
+            FROM rawSalesFetchData as rawSales
+
+            INNER JOIN `transformedData`.`transformHeader` as transHeader
+            ON transHeader.uniqueOrderId = rawSales.uniqueOrderId
+
+            inner join `rawDataTest`.`iikoStoreMapping` as iikoStoreMapping 
+            on rawSales.store = iikoStoreMapping.storeName
+
+            inner join `rawDataTest`.`iikoBrandMapping` as iikoBrandMapping
+            on rawSales.concept = iikoBrandMapping.conceptionName
+
+            WHERE transHeader.salesTransformed = 0
+            ;
+        """
+
+        transformSalesFetchData = pd.read_sql(query, conn)
+
+        transformSalesFetchData_toList = list(tuple(row)
+                                              for row in transformSalesFetchData.values)
+
+        insert_Query = """
+            INSERT INTO `transformedData`.`transformSales`
+            (
+                `uniqueOrderId`,
+                `store`,
+                `concept`,
+                `accountingDay`,
+                `receiptNo`,
+                `itemCode`,
+                `itemId`,
+                `itemName`,
+                `itemFullName`,
+                `itemAccountingCategory`,
+                `itemCategory`,
+                `itemGroup`,
+                `courseNo`,
+                `itemWaiter`,
+                `itemServicePrinting`,
+                `itemComment`,
+                `soldWithItem`,
+                `discountType`,
+                `surchargeType`,
+                `cookingFinishedAt`,
+                `numberOfItems`,
+                `averagePriceBeforeDiscount`,
+                `averagePrice`,
+                `grossSalesBD`,
+                `discountAmount`,
+                `surchargeAmountAED`,
+                `grossSalesAD`,
+                `effectiveDiscount`,
+                `VAT`,
+                `netSales`,
+                `cookingTimeAverage`,
+                `cookingTimeAverage1`,
+                `cookingTimeAverage2`,
+                `cookingTimeAverage3`,
+                `cookingTimeAverage4`,
+                `storeNo`,
+                `conceptionNo`
+            )
+            VALUES
+            (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+        cursor.executemany(insert_Query, transformSalesFetchData_toList)
+        conn.commit()
+        print("INSERTED INTO transform Sales")
+
+        updateQuery = f"""
+            UPDATE `transformedData`.`transformHeader` SET salesTransformed = True
+             WHERE uniqueOrderId IN {tuple(transformSalesFetchData.uniqueOrderId)};
+        """
+        cursor.execute(updateQuery)
+        conn.commit()
+        print("Update INTO transform Header's salesTransformed")
 
 
-"""
-iikoBrandMappingTransformData = pd.merge(
-    iikoStoreTransformData, iikoBrandMapping.conception, how='inner')
-
-iikoVirtualStoreMappingTransformData = iikoBrandMappingTransformData[(
-    iikoBrandMappingTransformData.store.isin(list(iikoVirtualStoreMapping.store))) & (
-    iikoBrandMappingTransformData.conception.isin(list(iikoVirtualStoreMapping.conception)))]
-
-
-print(iikoVirtualStoreMappingTransformData.dtypes)
-"""
+transformationRawData = TransformationRawData()
+transformationRawData.transformSales()
